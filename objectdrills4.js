227f9441e546287347026156136346979ad3c6ce
Ben Parman <benparman@gmail.com>
2017-11-01 13:40:10 -0600

    revised code for object drills 4 exercise

---
+++ objectdrills4.js
@@ -0,0 +1,11 @@
+const employees = [
+    {joe: "plumber"},
+    {bob: "baker"},
+    {carl: "cook"},
+    {gerald: "janitor"},
+    {paul: "gutter cleaner"}
+];
+
+for (i=0; i < employees.length; i++) {
+   console.log(employees[i]);
+}
